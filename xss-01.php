c934f6b05bf327aa0b7270f172c6f1c45c2a0f56
xx-ref <dizzlyrabbit666@gmail.com>
2022-06-13 14:40:24 +0800

    bulb

---
+++ xss-01.php
@@ -0,0 +1,7 @@
+<?php
+    echo "This is a testing page";
+    $xss = $_POST['x'];
+
+    echo $xss;
+
+?>
---
+++ xss.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Testing over</title>
+    </head>
+    <body>
+        <?php
+            echo $_REQUEST['name'];
+        ?>
+        <script>alert(1)</script>
+    </body>
+    
+</html>
